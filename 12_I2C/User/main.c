87d785125863fcfcd5dc0f275daf8058f6f453dd
xiaoyexuebiancheng <505223744@qq.com>
2020-10-25 12:12:48 +0800

    update

--- 12_I2C/User/main.c
+++ 12_I2C/User/main.c
@@ -7,6 +7,13 @@
 #include "bsp_usart.h"
 #include "bsp_dma_mtm.h"
 #include "bsp_dma_mtp.h"
+#include "bsp_i2c.h"
+
+
+#define  EEP_Firstpage      0x00
+uint8_t I2c_Buf_Write[256];
+uint8_t I2c_Buf_Read[256];
+uint8_t I2C_Test(void);
 
 //void Delay(uint32_t count)
 //{
@@ -16,8 +23,8 @@
 int main()
 {	
 //	uint8_t ch;
-	uint16_t i=0;
-	extern uint8_t SendBuff[SENDBUFF_SIZE];
+//	uint16_t i=0;
+//	extern uint8_t SendBuff[SENDBUFF_SIZE];
 //	uint8_t status =0;
 	
 	//配置LED灯
@@ -89,20 +96,83 @@ int main()
 //					break;
 //			}
 //		}
-	for(i=0;i<SENDBUFF_SIZE;i++)
-	{
-		SendBuff[i] = 'p';
-	}
+//	for(i=0;i<SENDBUFF_SIZE;i++)
+//	{
+//		SendBuff[i] = 'p';
+//	}
+//	
+//	USART_Config();
+//	USARTx_DMA_Config();
+//	USART_DMACmd(DEBUG_USARTx, USART_DMAReq_Tx, ENABLE);
+//	
+//	LED_G(ON);
 	
+	 /* 串口初始化 */
 	USART_Config();
-	USARTx_DMA_Config();
-	USART_DMACmd(DEBUG_USARTx, USART_DMAReq_Tx, ENABLE);
 	
-	LED_G(ON);
-	
+	printf("\r\n 这是一个I2C外设(AT24C02)读写测试例程 \r\n");
 
+	/* I2C 外设初(AT24C02)始化 */
+	I2C_EE_Init();
+	 	 
+  //EEPROM 读写测试
+	if(I2C_Test() ==1)
+	{
+			LED_G(ON);
+	}
+	else
+	{
+			LED_R(ON);
+	}
+  
+  while (1)
+  {      
+  }
 		
 }
 
+
+uint8_t I2C_Test(void)
+{
+	uint16_t i;
+
+	printf("写入的数据\n\r");
+    
+	for ( i=0; i<=255; i++ ) //填充缓冲
+  {   
+    I2c_Buf_Write[i] = i;
+
+    printf("0x%02X ", I2c_Buf_Write[i]);
+    if(i%16 == 15)    
+        printf("\n\r");    
+   }
+
+  //将I2c_Buf_Write中顺序递增的数据写入EERPOM中 
+	I2C_EE_BufferWrite( I2c_Buf_Write, EEP_Firstpage, 256);
+  
+  EEPROM_INFO("\n\r写成功\n\r");
+   
+   EEPROM_INFO("\n\r读出的数据\n\r");
+  //将EEPROM读出数据顺序保持到I2c_Buf_Read中
+	I2C_EE_BufferRead(I2c_Buf_Read, EEP_Firstpage, 256); 
+   
+  //将I2c_Buf_Read中的数据通过串口打印
+	for (i=0; i<256; i++)
+	{	
+		if(I2c_Buf_Read[i] != I2c_Buf_Write[i])
+		{
+			EEPROM_ERROR("0x%02X ", I2c_Buf_Read[i]);
+			EEPROM_ERROR("错误:I2C EEPROM写入与读出的数据不一致\n\r");
+			return 0;
+		}
+    printf("0x%02X ", I2c_Buf_Read[i]);
+    if(i%16 == 15)    
+        printf("\n\r");
+    
+	}
+  EEPROM_INFO("I2C(AT24C02)读写测试成功\n\r");
+  
+  return 1;
+}
 
 
